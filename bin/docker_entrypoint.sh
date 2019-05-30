e81918136057f84dafd657c18a4583db626213de
Danny Diekroeger <danny@bitgo.com>
2019-05-30 16:43:13 -0700

    remove rpcuser/pass from command line

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 BITCOIN_DIR=/bitcoin/.bitcoin
 BITCOIN_CONF=${BITCOIN_DIR}/bitcoin.conf
 
-exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF} -rpcuser=$(cat /rpcuser) -rpcpassword=$(cat /rpcpassword)
+exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF}
